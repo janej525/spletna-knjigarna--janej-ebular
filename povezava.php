a07aea16437bf5a5f1537f0db0819b9cabc391de
janej525 <janej.cebular@scv.si>
2026-05-22 11:14:55 +0200

    Add files via upload

---
+++ povezava.php
@@ -0,0 +1,9 @@
+<?php
+$host = "sql106.infinityfree.com";
+$user = "if0_41973754";
+$password = "TilQmRdGR";
+$database = "if0_41973754_spletna_knjigarna";
+        
+$link = mysqli_connect($host, $user, $password, $database)
+        or die("Povezava ni uspešna");
+
