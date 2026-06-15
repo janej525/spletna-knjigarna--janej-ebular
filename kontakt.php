8e2c095738a5a60e67feb507227b716cd73f28c1
janej525 <janej.cebular@scv.si>
2026-06-15 23:11:29 +0200

    Add files via upload

---
+++ kontakt.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <meta charset="utf-8">
+    <title>Kontakt</title>
+    <link href="still.css" rel="stylesheet">
+</head>
+<body>
+
+<?php require_once 'glava.php'; ?>
+
+<div class="stran-vsebina">
+    <div class="osnovna-sekcija kontakt-okvir">
+        <h1>Kontakt</h1>
+        <p>Ce imate vprasanje, nam lahko pisete.</p>
+
+        <div class="kontakt-podatki">
+            <p><b>Email:</b> janej@cebular.si</p>
+            <p><b>Telefon:</b> 068 667 101</p>
+            <p><b>Naslov:</b> SCV 1, 3320 Velenje</p>
+        </div>
+    </div>
+</div>
+
+<div id="footer">
+    <a href="info.php">Informacije</a>
+    <a href="kontakt.php">Kontakt</a>
+    <p>2026 Spletna knjigarna</p>
+</div>
+
+</body>
