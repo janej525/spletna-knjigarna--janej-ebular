0f3724900e270bbe4e46553998ef90a81b97e79e
janej525 <janej.cebular@scv.si>
2026-06-15 23:12:28 +0200

    Add files via upload

---
+++ info.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <meta charset="utf-8">
+    <title>Informacije</title>
+    <link href="still.css" rel="stylesheet">
+</head>
+<body>
+<?php require_once 'glava.php'; ?>
+
+<div class="sekcija sredina">
+    <h1>Informacije</h1>
+    <p>Spletna knjigarna je solsko izdelan projekt pri predmetu spletno programiranje.</p>
+    <p>Uporabnik lahko pregleda knjige, poisce knjigo, se registrira, prijavi, komentira knjigo in zabelezi prenos.</p>
+</div>
+
+</body>
+</html>
