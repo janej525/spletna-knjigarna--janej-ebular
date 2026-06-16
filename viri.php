98ed44b6a91ef3391bc764d3d4c7bb643326ddd0
janej525 <janej.cebular@scv.si>
2026-06-16 22:44:45 +0200

    Add files via upload

---
+++ viri.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <meta charset="utf-8">
+    <title>Viri</title>
+    <link href="still.css" rel="stylesheet">
+</head>
+<body>
+
+<?php require_once 'glava.php'; ?>
+
+<div class="sekcija sredina">
+    <h1>Viri</h1>
+    <p>
+        Za download :<br>
+        <a href="https://www.w3schools.com/tags/att_a_download.asp" target="_blank">
+            https://www.w3schools.com/tags/att_a_download.asp
+        </a>
+    </p>
+
+    <p>
+        Da gre na pravi id <br>
+        <a href="https://www.w3schools.com/php/php_superglobals_get.asp" target="_blank">
+            https://www.w3schools.com/php/php_superglobals_get.asp
+        </a>
+    </p>
+
+    <p>
+        Flex:<br>
+        <a href="https://www.w3schools.com/css/css3_flexbox.asp" target="_blank">
+            https://www.w3schools.com/css/css3_flexbox.asp
+        </a>
+    </p>
+
+    <p>
+        Flex direction:<br>
+        <a href="https://www.w3schools.com/cssref/css3_pr_flex-direction.php" target="_blank">
+            https://www.w3schools.com/cssref/css3_pr_flex-direction.php
+        </a>
+    </p>
+	<p>
+    ChatGPT - uporabljen kot pomoč pri razumevanju kode, popravljanju napak in oblikovanju noge spletne strani:<br>
+    <a href="https://chatgpt.com/" target="_blank">https://chatgpt.com/</a>
+</p>
+</div>
+
+<div id="footer">
+    <a href="info.php">Informacije</a>
+    <a href="kontakt.php">Kontakt</a>
+    <a href="viri.php">Viri</a>
+    <p>2026 Spletna knjigarna</p>
+</div>
+
+</body>
+</html>
